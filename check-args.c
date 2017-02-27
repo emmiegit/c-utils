745f11e8db46c337c6d7630f834c8c38dbe3ff14
Emmie Maeda <emmie.maeda@gmail.com>
2017-02-27 02:35:43 -0800

    Print last argv.

--- check-args.c
+++ check-args.c
@@ -6,6 +6,7 @@ int main(int argc, const char *argv[])
 
 	printf("argc: %d\n", argc);
 	for (i = 0; i < argc; i++)
-		printf("argv[%d] = %s\n", i, argv[i]);
+		printf("argv[%d] = \"%s\"\n", i, argv[i]);
+	printf("argv[%d] = %p\n", i, (void *)argv[i]);
 	return 0;
 }
