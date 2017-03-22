a7301c9944837cd139f75de020d4d29bee24f02e
Emmie Maeda <emmie.maeda@gmail.com>
2017-03-21 19:15:37 -0700

    Add sum-primes.

---
+++ sum-primes.c
@@ -0,0 +1,88 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static long *primes;
+static size_t len;
+
+static void fill_array(void)
+{
+	size_t i;
+
+	for (i = 0; i < len; i++)
+		primes[i] = i + 1;
+}
+
+static void sieve_eratosthenes(void)
+{
+	size_t n;
+
+	for (n = 2; n < (len / 2) + 1; n++) {
+		size_t i;
+
+		for (i = n + 1; i < len; i++) {
+			if (primes[i] % n == 0)
+				primes[i] = -1;
+		}
+	}
+}
+
+static void print_primes(void)
+{
+	size_t i;
+
+	for (i = 0; i < len; i++) {
+		if (primes[i] > 0)
+			printf("%ld, ", primes[i]);
+	}
+	putchar('\n');
+}
+
+static long sum_primes(void)
+{
+	size_t i;
+	long sum;
+
+	sum = 0;
+	for (i = 0; i < len; i++) {
+		if (primes[i] > 0)
+			sum += primes[i];
+	}
+	return sum;
+}
+
+/* Usage: sum-primes [MAX-VALUE] */
+int main(int argc, const char *argv[])
+{
+	long sum;
+
+	if (argc > 1) {
+		len = atol(argv[1]);
+		if (len <= 0) {
+			fprintf(stderr, "%s: max-value must be a positive, nonzero integer, not '%s'\n",
+				argv[0], argv[1]);
+			return 1;
+		}
+	} else {
+		len = 500;
+	}
+
+	primes = malloc(len * sizeof(long));
+	if (!primes) {
+		fprintf(stderr, "%s: unable to allocate array: %s\n",
+			argv[0], strerror(errno));
+		return 1;
+	}
+
+	fill_array();
+	sieve_eratosthenes();
+	print_primes();
+	sum = sum_primes();
+
+	printf("Sum of all primes below %zu is %ld.\n",
+		len, sum);
+
+	free(primes);
+	return 0;
+}
